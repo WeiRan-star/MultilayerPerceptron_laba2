c488cf51a5e3a933743c3f1617bc55c4dcf97661
sidav <sidav94@gmail.com>
2017-11-06 20:27:19 +0300

    Successful read of the PGM file.

--- DataCollector.py
+++ DataCollector.py
@@ -1,27 +1,35 @@
 #this should return X and Y teaching sets.
+from PIL import Image
+import numpy
+pil_currentfile = Image.open("faces/s1/1.pgm")
+cfile = open("faces/s1/1.pgm", 'r')
 
-cfile = open("faces/test.pgm", 'r')#("faces/s1/1.pgm", 'r')
 
 def read_pgm(pgmf):
-    #Return a raster of integers from a PGM as a list of lists.
-    assert pgmf.readline() == 'P5\n'
-    (width, height) = [int(i) for i in pgmf.readline().split()]
-    depth = int(pgmf.readline())
-    assert depth == 255
+    fuck = numpy.array(pgmf)
+    print(fuck.tolist())
 
-    print("width={0}, height={1}, depth={2}".format(width, height, depth))
 
-    raster = []
-    schet = 0
-    for y in range(height):
-        row = []
-        for y in range(width):
-            schet+=1
-            currbyte = pgmf.read(1)
-            print(currbyte)
-            if currbyte == '':
-                print("Oh noes! {0}th byte is a bullshit!".format(schet))
-                currbyte = chr(0)
-            row.append(ord(currbyte))
-        raster.append(row)
-    return raster
+# complete bullshit:
+# def read_pgm(pgmf):
+#     #Return a raster of integers from a PGM as a list of lists.
+#     assert pgmf.readline() == 'P5\n'
+#     (width, height) = [int(i) for i in pgmf.readline().split()]
+#     depth = int(pgmf.readline())
+#     assert depth == 255
+#
+#     print("width={0}, height={1}, depth={2}".format(width, height, depth))
+#
+#     raster = []
+#     schet = 0
+#     for y in range(height):
+#         row = []
+#         for y in range(width):
+#             schet+=1
+#             currbyte = pgmf.read(1)
+#             print(currbyte)
+#             if currbyte == '':
+#                 currbyte = chr(10)
+#             row.append(ord(currbyte))
+#         raster.append(row)
+#     return raster
